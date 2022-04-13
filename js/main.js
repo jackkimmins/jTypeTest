8ff2a63430a781c3a40387afcfacca6728ad1c5f
Jack Kimmins <50149309+jackkimmins@users.noreply.github.com>
2022-04-13 10:16:39 +0100

    Tweaked the text display system.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 let isCountingDown = false;
 let currentWord = null;
+let currentWordText = null;
+let currentWordChars = [];
 let correct = 0;
 let incorrect = 0;
 
@@ -69,8 +71,6 @@ $.ajax({
     url: `./data/${wordList}.txt`,
     type: 'GET',
     success: function (response) {
-        // console.log(response);
-
         let words = [];
 
         if (wordList == "sentences")
@@ -84,12 +84,38 @@ $.ajax({
 
         //Add words to .words
         words.forEach(word => {
-            $('.words').append(`<span class="word">${word}</span>`);
+            //Split word into an array of letters
+            let letters = word.split("");
+
+            //Create a element
+            let wordElement = document.createElement("span");
+            wordElement.classList.add("word");
+            
+            for (let i = 0; i < letters.length; i++)
+            {
+                //Check if the letter is empty
+                if (letters[i].trim() == "")
+                    continue;
+
+                //Create a letter element
+                let letterElement = document.createElement("span");
+                letterElement.classList.add("letter");
+                letterElement.innerHTML = letters[i];
+
+                //Add the letter element to the word element
+                wordElement.appendChild(letterElement);
+            }
+
+            $('.words').append(wordElement);
         });
 
         //Get the first element of .words
         currentWord = $('.words .word').first();
         currentWord.addClass('active');
+        currentWordChars = currentWord.text().split("");
+
+        $('#caret').css('top', currentWord.position().top + 'px');
+        $('#caret').css('left', currentWord.position().left + 'px');
 
         //Set .container to display block important
         $('#loader').css("display", "none");
@@ -125,20 +151,54 @@ $("#textInput").keyup(function(event) {
     {
         CountDown(60);
         isCountingDown = true;
+        $('#caret').removeClass('blink');
         $("#textInput").prop("disabled", false);
-        return;
     }
 
     const value = $("#textInput").val().trim();
+    const valueChars = value.split("");
+    const valueCharsLength = valueChars.length;
 
-    //Check if the current word contains value
-    if (currentWord.text().includes(value))
+    //Select the element of the last letter in the array
+    const lastChar = currentWord.find('.letter').eq(valueCharsLength);
+
+    if (lastChar.length != 0)
     {
-        currentWord.removeClass('incorrect');
+        const lastCharTopPos = lastChar.position().top;
+        const lastCharLeftPos = lastChar.position().left;
+
+        $('#caret').css('top', currentWord.position().top + lastCharTopPos + 'px');
+        $('#caret').css('left', currentWord.position().left + lastCharLeftPos + 'px');
     }
-    else
+    else if (valueCharsLength <= currentWordChars.length)
     {
-        currentWord.addClass('incorrect');
+        const lastChar2 = currentWord.find('.letter').eq(valueCharsLength - 1);
+        $('#caret').css('top', currentWord.position().top + lastChar2.position().top + 'px');
+        $('#caret').css('left', currentWord.position().left + lastChar2.position().left + lastChar2.width() + 'px');
+    }
+
+    for (let i = 0; i < currentWordChars.length; i++)
+    {
+        const letter = currentWord.find(".letter").eq(i);
+
+        if (valueCharsLength <= i)
+        {
+            letter.removeClass("correct");
+            letter.removeClass("incorrect");
+            continue;
+        }
+
+        //Set correct on the letter if the letter is correct
+        if (valueChars[i] == currentWordChars[i])
+        {
+            letter.addClass("correct");
+            letter.removeClass("incorrect");
+        }
+        else
+        {
+            letter.addClass("incorrect");
+            letter.removeClass("correct");
+        }
     }
 
     //Check if the key is space
@@ -147,10 +207,18 @@ $("#textInput").keyup(function(event) {
         //If value is empty
         if (value == "")
         {
+            $(this).val("");
             return;
         }
 
-        if (value == currentWord.text())
+        if (valueCharsLength <= currentWordChars.length)
+        {
+            const lastChar2 = currentWord.find('.letter').eq(valueCharsLength - 1);
+            $('#caret').css('top', currentWord.position().top + lastChar2.position().top + 'px');
+            $('#caret').css('left', currentWord.position().left + lastChar2.position().left + lastChar2.width() + document.documentElement.clientWidth * 0.65 / 100 + 'px');
+        }
+
+        if (value == currentWordText)
         {
             currentWord.addClass('correct');
             correct++;
@@ -168,10 +236,13 @@ $("#textInput").keyup(function(event) {
         let lastWordPosition = currentWord.position().top;
 
         currentWord = currentWord.next();
+        currentWordChars = currentWord.text().split("");
+        currentWordText = currentWord.text().replace(/\r?\n|\r/g, "");
 
         let currentWordPosition = currentWord.position().top;
 
-        if (currentWordPosition != lastWordPosition) {
+        if (currentWordPosition != lastWordPosition)
+        {
             let wordsToRemove = [];
 
             $('.word').each(function() {
@@ -180,6 +251,10 @@ $("#textInput").keyup(function(event) {
             });
 
             wordsToRemove.forEach(word => word.remove());
+
+            const lastChar2 = currentWord.find('.letter').eq(0);
+            $('#caret').css('top', currentWord.position().top + lastChar2.position().top + 'px');
+            $('#caret').css('left', currentWord.position().left + lastChar2.position().left + 'px');
         }
 
         currentWord.addClass('active');
